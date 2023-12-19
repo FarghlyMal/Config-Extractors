374a3fdee3bf927b7dceed680736aae95019edb6
FarghlyMal <133105828+FarghlyMal@users.noreply.github.com>
2023-12-20 00:04:41 +0200

    Update Config_Extractor.py

--- Qbot_Dec_2023/Config_Extractor.py
+++ Qbot_Dec_2023/Config_Extractor.py
@@ -3,7 +3,6 @@ import hashlib
 import binascii
 import pefile
 path=r"file path"
-#from Header_file import encrypted_blob,encrypted_data,input_data
 def get_data():
     pe=pefile.PE(path)
     key_val=b''
@@ -18,7 +17,6 @@ def get_data():
             key_val=section.get_data()[157696:157696 + key_val_length]
             AES_encrypted=section.get_data()[157488 :157488 + AES_encrypted_len]
             blob_data= section.get_data()[157872 : 157872 + blob_data_len]
-            #print("we got it")
     return AES_encrypted,blob_data,key_val
 def get_data_2():
     pe=pefile.PE(path)
@@ -48,34 +46,24 @@ def get_data_3():
             key_val_3=section.get_data()[156416 : 156416 + key_val_3_len]
             AES_encrypted_3=section.get_data()[156512 : 156512 + AES_encrypted_3_len]
             blob_data_3=section.get_data()[156392 : 156392 + blob_data_3_len]
-            #print(section.get_data().find(pattern))
-            #print(AES_encrypted_3)
     return AES_encrypted_3,blob_data_3,key_val_3    
   
 def get_original_length(unpadded_data):
-    # The last byte of PKCS7-padded data indicates the padding length
     padding_length = unpadded_data[-1]
     
-    # The original length is the total length minus the padding length
     original_length = len(unpadded_data) - padding_length
     return original_length
   
 def get_original_length(unpadded_data):
-    # The last byte of PKCS7-padded data indicates the padding length
     padding_length = unpadded_data[-1]
     
-    # The original length is the total length minus the padding length
     original_length = len(unpadded_data) - padding_length
     return original_length
 
 def decrypt_data_using_AES(encrypted_data, session_key):
-    # The first 16 bytes of the encrypted data are used as the IV
     iv = encrypted_data[:16]
-    # Create an AES cipher object in CBC mode with the derived 
-    #  key and IV
     cipher = AES.new(session_key, AES.MODE_CBC, iv)
     encrypted_data=encrypted_data[16:]
-    # Decrypt the remaining bytes of the encrypted data
     decrypted_data = cipher.decrypt(encrypted_data)
     original_length = get_original_length(decrypted_data)
     return decrypted_data,original_length
@@ -97,8 +85,7 @@ def mw_decryypt(decrypted_AES_Data,AES_d_length,encrypted_blob_,index_val,max_in
         xor_val =encrypted_blob_[index_val + i] ^ decrypted_AES_Data[(index_val +i) % AES_d_length]
         decrypted_string+=chr(xor_val)
     return str(decrypted_string)
-# Example usage:
-#input_data = b'This is the data for session key derivation.'
+    
 def decrypt_1():
     encrypted_data,encrypted_blob,key_to_be_hashed = get_data()
     session_key = derive_session_key(key_to_be_hashed)
